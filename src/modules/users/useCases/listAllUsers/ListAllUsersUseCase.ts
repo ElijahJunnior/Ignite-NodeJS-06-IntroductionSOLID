1b716d05b7f6b016001550a3b85d75a7c0bf123a
Elias Junior <elijah.junnior@outlook.com>
2022-07-08 18:13:19 -0300

    fix issue on user repository

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,8 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // const userRequesting = this.usersRepository.findById(user_id);
-    // if(!userRequesting) then
+    const userRequesting = this.usersRepository.findById(user_id);
+
+    if (!userRequesting) {
+      throw new Error("User not exists");
+    }
+
+    if (!userRequesting.admin) {
+      throw new Error("User not have permission");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
